b6b8fae5f5942cbcd00f50425d2520844940eb27
SamuelPalmhager <palmhagersamuel@gmail.com>
2025-11-28 10:54:54 +0100

    Initial commit for dll

--- dll.cpp
+++ dll.cpp
@@ -1,1 +1,74 @@
+#include "dll.h"
+#include <stdio.h>
+#include <memory>
+#include <iostream>
 
+dll_t* get_new_dll(){
+
+    dll_t *dll = new dll_t;
+
+    dll->head = new dll_node_t;
+    dll->head->data = 1;
+    dll->head->next = nullptr;
+    dll->head->prev = nullptr;
+
+    return dll;
+};
+
+int add_data_to_dll(dll_t* dll, void* app_data){
+
+    //We need to check if we should add in the beginning or end of the list
+    
+    if(!dll || !dll->head) return 1;
+
+      //Assign to heads next node
+
+        dll_node_t *new_node = (dll_node_t*)app_data;
+        new_node->next = nullptr;
+        new_node->prev = nullptr;
+
+
+    if(dll->head->next == nullptr){
+
+        dll->head->next = new_node;
+        new_node->next = nullptr; //Maybe redundant
+        new_node->prev = dll->head;
+
+        std::cout << "Added to start of dll" << std::endl;
+
+        return 0;
+
+    }
+
+        dll_node_t *temp = dll->head;
+        int i {0};
+
+        while(temp->next != nullptr){
+            std::cout << "Stepping in " << i << std::endl;
+            temp = temp->next;
+        }
+
+        //Assume we have reached end of dll.
+
+        temp->next = new_node;
+        new_node->next = nullptr; //Maybe redundant
+        new_node->prev = temp;
+        return 0;
+
+};
+
+void print_dll(dll_t* dll){
+
+    if(!dll || !dll->head) return;
+
+    dll_node_t *temp {dll->head->next};
+    int i {0};
+
+    while(temp != nullptr){
+        std::cout << "Data for node: " << i << " is " << (int)(temp->data) << std::endl;
+        temp = temp->next;
+        i++;
+    }
+    std::cout << "Reached end of dll" << std::endl;
+
+}
--- dll.h
+++ dll.h
@@ -1,1 +1,39 @@
+#ifndef _DOUBLY_LINKED_LIST_
+#define _DOUBLY_LINKED_LIST_
 
+typedef struct dll_node_{
+
+    int data;
+    dll_node_ *next;
+    dll_node_ *prev;
+
+}dll_node_t;
+
+typedef struct dll_{
+
+    dll_node_t *head;
+
+}dll_t;
+
+
+/**
+ * Function to get a new dll;
+ * @return dll_t*, pointer to beginning of new dll.
+ */
+
+dll_t* get_new_dll();
+
+/**
+ * Function to add new data to dll, will add in the beginning or end of dll
+ * @param dll pointer to dll we will add data to. @param app_data pointer to new node which should be added
+ * @return will return 0 if successful, or 1 if not successful. 
+ */
+
+int add_data_to_dll(dll_t* dll, void* app_data);
+
+
+/**
+ * Function to print dll
+ */
+void print_dll(dll_t* dll);
+#endif //_DOUBLY_LINKED_LIST_
